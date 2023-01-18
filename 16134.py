dbf3ecbd701fa97c6928df7c02a124eacbed36ba
Chanwoong1 <jcw7468@gmail.com>
2023-01-18 15:51:29 +0900

    feat: 16134

---
+++ 16134.py
@@ -0,0 +1,12 @@
+# 조합(Combination)
+# https://www.acmicpc.net/problem/16134
+
+N, K = map(int, input().split())
+p = 10 ** 9 + 7
+a = b = 1
+for i in range(min(K, N - K)):
+    a *= N - i; a %= p
+    b *= i + 1; b %= p
+invb = pow(b, p - 2, p)
+ans = a * invb % p
+print(ans)
